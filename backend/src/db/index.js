203532e9d8fa1442ef42f5470124c0ce638b9dff
aguiartws <luisfilippi.aguiar@gmail.com>
2025-11-11 17:44:38 -0300

    .

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',      
   user: 'root',
-  password: '@Luis232231',      
-  database: 'biblioteca_universitaria', 
+  password: 'Belinha18-18',      
+  database: 'biblioteca', 
   port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
