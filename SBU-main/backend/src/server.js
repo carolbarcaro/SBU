a1da2b5c07f807461c7580e3d6be259e3cd0bb59
Beatriz Caroline Moreno Tavares <beatriz.cmt@puccampinas.edu.br>
2025-11-25 21:35:12 -0300

    apagando a SBU errada

--- SBU-main/backend/src/server.js
+++
@@ -1,8 +0,0 @@
-  // backend/src/server.js
-  const app = require('./app');
-
-  const PORT = process.env.PORT || 3000;
-
-  app.listen(PORT, () => {
-    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
-  });
